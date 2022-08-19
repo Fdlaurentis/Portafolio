b61844bca367f3d1df9593ad152bfe7eaf7bcb4a
fdlaurentis <fdlaurentis@gmail.com>
2022-08-19 12:00:30 -0500

    'formularioDeContact'

--- src/componentes/Contactame.jsx
+++ src/componentes/Contactame.jsx
@@ -4,14 +4,16 @@ const Contactame = () => {
     return (
         <div className='divBlock'>
             <h1>Contactame</h1>
-            <form className='formulario' name='contact'  method="POST" data-netlify="true">
-                <label htmlFor="nameEmpresa">Nombre de la Empresa</label>
-                <input type="text" id="nameEmpresa" name='nameEmpresa'/>
-                <label htmlFor="emailEmpresa">Email de Empresa</label>
-                <input type="email" id="emailEmpresa" name='emailEmpresa'/>
-                <label htmlFor="descEmpresa">Descripción del cargo</label>
-                <textarea id="descEmpresa" name='descEmpresa' cols="30" rows="20" style={{height:'150px'}}></textarea>
-                <button>Enviar</button>
+            <form name="contact" netlify>
+                <p>
+                    <label>Name <input type="text" name="name" /></label>
+                </p>
+                <p>
+                    <label>Email <input type="email" name="email" /></label>
+                </p>
+                <p>
+                    <button type="submit">Send</button>
+                </p>
             </form>
         </div>
     );
