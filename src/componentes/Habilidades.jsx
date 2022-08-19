2ff03b9f5d52c6f0db3e763888d1497bd45b2c69
fdlaurentis <fdlaurentis@gmail.com>
2022-08-19 12:51:55 -0500

    'update'

--- src/componentes/Habilidades.jsx
+++ src/componentes/Habilidades.jsx
@@ -2,16 +2,48 @@ import React from 'react';
 import css from '../images/css-3.png'
 import html from '../images/html-5.png'
 import js from '../images/js.png'
-import vB from '../images/visual-basico.png'
 import sql from '../images/servidor-sql.png'
 import react from '../images/structure.png'
+import { Card } from 'react-bootstrap'
 
 const Habilidades = () => {
     return (
         <div className='container'>
-            <div className="divBlock" id='herramientas'>
-                <h4>Habilidates Tecnicas</h4>
-                
+            <div className="divHabi">
+                <h1>Habilidates Tecnicas</h1>
+                <Card style={{ background: '#04076d', width: '2in'}} className='cardHab'>
+                    <Card.Img variant="top" src={react} style={{ width: '80px' }} />
+                    <Card.Body>
+                        <Card.Title style={{ textAlign: 'center' }}><h6>React</h6></Card.Title>
+                        <div className="contCard">
+                            <Card.Text style={{ color: '#f2d43e' }}>
+                                Redux Axios Hook-DOM Componentes
+                            </Card.Text>
+                        </div>
+                    </Card.Body>
+                </Card>
+                <Card style={{ background: '#04076d', width: '2in' }} className='cardHab'>
+                    <Card.Img variant="top" src={js} style={{ width: '80px' }} />
+                    <Card.Body>
+                        <Card.Title style={{ textAlign: 'center' }}><h6>JavaScritp</h6></Card.Title>
+                        <div className="contCard">
+                            <Card.Text style={{ color: '#f2d43e' }}>
+                                Closures Destructuring sintaxis extendida Parámetros rest Callback Functions Promesas Async Await
+                            </Card.Text>
+                        </div>
+                    </Card.Body>
+                </Card>
+                <Card style={{ background: '#04076d', width: '2in' }} className='cardHab'>
+                    <Card.Img variant="top" src={html} style={{ width: '80px' }} />
+                    <Card.Body>
+                        <Card.Title style={{ textAlign: 'center' }}><h6>HTML</h6></Card.Title>
+                        <div className="contCard">
+                            <Card.Text style={{ color: '#f2d43e' }}>
+                            Atributos requerido Atributos opcionales Atributos globales
+                            </Card.Text>
+                        </div>
+                    </Card.Body>
+                </Card>
             </div>
         </div>
     );
