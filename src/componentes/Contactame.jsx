d08eca32bb0ea6663c658195f87433592a8c8771
fdlaurentis <fdlaurentis@gmail.com>
2022-08-23 10:48:54 -0500

    updateFormularioContacto

--- src/componentes/Contactame.jsx
+++ src/componentes/Contactame.jsx
@@ -1,22 +1,43 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
 
 const Contactame = () => {
-    return (
-        <div className='container'>
-            <h1>Contactame</h1>
-            <form name="contact" method="POST" data-netlify="true">
-                <p>
-                    <label>Name <input type="text" name="name" /></label>
-                </p>
-                <p>
-                    <label>Email <input type="email" name="email" /></label>
-                </p>
-                <p>
-                    <button type="submit">Send</button>
-                </p>
-            </form>
+    const navigate=useNavigate()
+
+  return (
+    <div className="container">
+      <h1>Contactame</h1>
+      <form action="https://formsubmit.co/9b00cc1265535c9bd393bc7d3378483d" method="POST">
+        <div>
+          <label>
+            Name <input type="text" name="name" />
+          </label>
         </div>
-    );
+        <div>
+          <label>
+            Email <input type="email" name="email" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Asunto <input type="text" name="asunto" />
+          </label>
+        </div>
+        <div>
+          <textarea
+            name="contenido"
+            style={{ width: "273.2px", height: "150px" }}
+          ></textarea>
+        </div>
+        <div>
+          <input type="submit" value='Enviar' name='_next' onClick={()=>navigate('/')}/>
+        </div>         
+        <input type="hidden" name='_captcha' value='false'/>
+      </form>
+      
+    </div>
+  );
 };
 
 export default Contactame;
