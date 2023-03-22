286319b897dd19c6aac57c817a4318ea94813bf6
fdlaurentis <fdlaurentis@gmail.com>
2023-03-22 15:58:17 -0500

    Update Style contact

--- src/page/Contact/Contact.jsx
+++ src/page/Contact/Contact.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react"
 import Cards from "../../componentes/Cards/Cards"
 import style from './Contact.module.css'
 
 const Contact = ({ theme, language }) => {
+
+    const [validateName, setValidateName] = useState('')
+    const [validateEmail, setValidateEmail] = useState('')
+    const [validateMsj, setValidateMsj] = useState('')
 
     return (
         <div className="main">
@@ -23,29 +28,43 @@ const Contact = ({ theme, language }) => {
                 <div
                     className={style.input}
                 >
-                    <input type="text" className={style.textInput}/>
+                    <input
+                        type="text"
+                        className={style.textInput}
+                        onChange={(e) => setValidateName(e.target.value)}
+                        value={validateName}
+                    />
                     <i className="fa-solid fa-user"></i>
                     <span
-                        className={style.span}
+                        className={validateName ? style.spanValited : style.span}
                         style={{ background: theme ? '#ffffff' : '#52bd89' }}
                     >Nombre</span>
                 </div>
                 <div
                     className={style.input}
                 >
-                    <input type="text" className={style.textInput}/>
+                    <input
+                        type="text"
+                        className={style.textInput}
+                        onChange={(e) => setValidateEmail(e.target.value)}
+                        value={validateEmail}
+                    />
                     <i className="fa-solid fa-envelope"></i>
                     <span
-                        className={style.span}
+                        className={validateEmail ? style.spanValited : style.span}
                         style={{ background: theme ? '#ffffff' : '#52bd89' }}
                     >Correo</span>
                 </div>
                 <div
                     className={style.input}
+                    onChange={(e) => setValidateMsj(e.target.value)}
+                    value={validateMsj}
                 >
-                    <textarea className={style.textInput}></textarea>
+                    <textarea
+                        className={style.textInput}
+                    ></textarea>
                     <span
-                        className={style.span}
+                        className={validateMsj ? style.spanValited : style.span}
                         style={{ background: theme ? '#ffffff' : '#52bd89' }}
                     >Mensaje</span>
                 </div>
